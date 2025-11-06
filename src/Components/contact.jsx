1a0066ce14c27d6a1e25009e3e138266f26ef6b9
BHAVYA <bhavy1921@gmail.com>
2025-11-06 20:48:38 +0530

    Update the route url to local backend and render

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -25,7 +25,11 @@ export const Contact = () => {
     e.preventDefault();
     setButtonText("Sending...");
 
-    let response = await fetch("https://portfolio-backend-1b5p.onrender.com/contact", {
+    let response = await fetch(`${
+    import.meta.env.MODE === "development"
+      ? "http://localhost:4500"
+      : "https://portfolio-backend-1b5p.onrender.com"
+  }/contact`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formDetails),
